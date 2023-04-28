e21080203fbffe6f56f2188a1f64318a4b739a90
RobertSO84 <robert.oropeza@gmail.com>
2023-04-28 16:43:35 -0600

    First commit

---
+++ bin/cdk-starter.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkStarterStack } from '../lib/cdk-starter-stack';
+import { PhotosStack } from '../lib/PhotosStack';
+import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+
+const app = new cdk.App();
+// new CdkStarterStack(app, 'CdkStarterStack' );
+new PhotosStack(app, 'PhotosStack' );
+new PhotosHandlerStack(app, 'PhotosHandlerStack' );
